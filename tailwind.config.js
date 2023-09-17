817d305fe87af4c6d96cd617103a7630373a5ab1
Damian Adamus <adamus.damian@yahoo.com>
2023-09-17 13:57:36 +0200

    new config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+module.exports = {
 	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		".src/pages/**/*{js,ts,jsx,tsx,mdx}",
+		".src/components/**/*{js,ts,jsx,tsx,mdx}",
+		".src/app/**/*{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
 		extend: {
@@ -16,4 +14,3 @@ const config: Config = {
 	},
 	plugins: [],
 };
-export default config;
